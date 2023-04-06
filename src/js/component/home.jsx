2d13ec986a49d520e69ca4fcab02db23ad400646
SalZeus <juanfeguto2@gmail.com>
2023-04-06 03:31:50 +0000

    last modifications

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -18,31 +18,35 @@ const Home = () => {
 		setTodos(newTodos)
 	}
 	return (
-		<div className="card">
-			<div className="card-header">
-			<div className="mb-3">
-  				<input type="email"
-				 className="form-control border-0" 
-				 id="exampleFormControlInput1" 
-				 placeholder="Escribe una tarea"
-				 value={task}
-				 onChange={(e) => setTask(e.target.value)}
-				 onKeyDown={addTask}
-				 />
+		<>
+	
+			<p class="fs-1 text-center">ToDos</p>
+			<div className="card">
+				<div className="card-header">
+				<div className="mb-3">
+					<input type="email"
+					className="form-control border-0" 
+					id="exampleFormControlInput1" 
+					placeholder="Escribe una tarea"
+					value={task}
+					onChange={(e) => setTask(e.target.value)}
+					onKeyDown={addTask}
+					/>
+				</div>
+				</div>
+				<ul className="list-group">
+					{todos.map((todo, index) =>(
+						<li key={index} className="item list-group-item d-flex justify-content-between align-items-center">
+						{todo}
+						<button onClick={()=> delTask(index)} type="button" className="btn btn-sm rounded-pill btn-outline-danger">x</button>
+						</li>
+					))}	
+				</ul>
+				<div className="card-footer">
+					{todos.length} tasks left m8 <strong>les goo!!</strong>
+				</div>
 			</div>
-			</div>
-			<ul className="list-group">
-				{todos.map((todo, index) =>(
-					<li key={index} className="item list-group-item d-flex justify-content-between align-items-center">
-					{todo}
-					<button onClick={()=> delTask(index)} type="button" className="btn btn-sm rounded-pill btn-outline-danger">x</button>
-					</li>
-				))}	
-			</ul>
-			<div className="card-footer">
-  				 {todos.length} tasks left m8 <strong>les goo!!</strong>
- 			</div>
-			</div>
+		</>
 	);
 };
 
